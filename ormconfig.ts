3f9c3d5d9862e4050da4edc9ace483dfce09f152
ngxquang <baophong2401@gmail.com>
2024-09-23 12:27:58 +0700

    fix(db) fix config database

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,29 +2,29 @@ import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOpti
 
 const config: MysqlConnectionOptions = {
   type: 'mysql',
-  // replication: {
-  //   master: {
-  //     host: process.env.MYSQL_MASTER_HOST,
-  //     port: +process.env.MYSQL_MASTER_PORT,
-  //     username: process.env.MYSQL_MASTER_USER,
-  //     password: process.env.MYSQL_MASTER_PASSWORD,
-  //     database: process.env.MYSQL_MASTER_DATABASE,
-  //   },
-  //   slaves: [
-  //     {
-  //       host: process.env.MYSQL_SLAVE_HOST,
-  //       port: +process.env.MYSQL_SLAVE_PORT,
-  //       username: process.env.MYSQL_SLAVE_USER,
-  //       password: process.env.MYSQL_SLAVE_PASSWORD,
-  //       database: process.env.MYSQL_SLAVE_DATABASE,
-  //     },
-  //   ],
-  // },
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Admin123',
-  database: 'ngxquang',
+  replication: {
+    master: {
+      host: process.env.MYSQL_MASTER_HOST,
+      port: +process.env.MYSQL_MASTER_PORT,
+      username: process.env.MYSQL_MASTER_USER,
+      password: process.env.MYSQL_MASTER_PASSWORD,
+      database: process.env.MYSQL_MASTER_DATABASE,
+    },
+    slaves: [
+      {
+        host: process.env.MYSQL_SLAVE_HOST,
+        port: +process.env.MYSQL_SLAVE_PORT,
+        username: process.env.MYSQL_SLAVE_USER,
+        password: process.env.MYSQL_SLAVE_PASSWORD,
+        database: process.env.MYSQL_SLAVE_DATABASE,
+      },
+    ],
+  },
+  // host: 'localhost',
+  // port: 3306,
+  // username: 'root',
+  // password: 'Admin123',
+  // database: 'ngxquang',
   synchronize: true,
   logging: true,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
